8b8f4deddb9243b7d35674fb83e7e9aff0b1202b
Frank Kuligowski <fckuligowski>
2019-08-16 13:51:57 -0500

    finished tutorial

---
+++ example.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  profile = "default"
+  region = var.region 
+}
+
+resource "aws_instance" "example" {
+  ami = var.amis[var.region]
+  instance_type = "t2.micro"
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
+}
+
+resource "aws_eip" "ip" {
+  vpc = true
+  instance = aws_instance.example.id
+}
+
+output "ip" {
+  value = aws_eip.ip.public_ip
+}
+
+terraform {
+  backend "remote" {
+    organization = "FrankK"
+
+    workspaces {
+      name = "Tutorial"
+    }
+  }
+}
